ffbcdeb54ab2e434c9df459bcd0f098952a9338b
Raphael-Bezerra <raphabzerra15@gmail.com>
2022-05-17 22:13:26 -0300

    sql

---
+++ script.sql
@@ -0,0 +1,24 @@
+CREATE TABLE cliente(
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    data_nasc DATE NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE
+);
+
+CREATE TABLE veiculo(
+    placa VARCHAR(7) PRIMARY KEY,
+    fabricante VARCHAR(30) NOT NULL,
+    modelo VARCHAR(20) NOT NULL,
+    ano INT NOT NULL,
+    cor VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE locacao(
+    nr_locacao INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT NOT NULL,
+    placa VARCHAR(7) NOT NULL,
+    CONSTRAINT fk_locacao_veiculo FOREIGN KEY(placa) 
+    	REFERENCES veiculo(placa),
+    CONSTRAINT fk_locacao_cliente FOREIGN KEY(id_cliente) 
+    	REFERENCES cliente(id_cliente)
+);
